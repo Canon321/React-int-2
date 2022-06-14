0a6d4a83bb71bf6a0cea47dad2fd26371b7ffde0
Vincent BRIAND <vincent-briand_student2022@wilder.school>
2022-06-14 14:08:27 +0200

    github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import QuoteCard from "./components/QuoteCard";
 
 function App() {
+  const [simpsonsOnly, setSimpsonsOnly] = useState(false);
+  const [quoteList, setQuoteList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes?count=10")
+      .then((res) => {
+        setQuoteList(res.data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setSimpsonsOnly(!simpsonsOnly)}>
+        Simpsons family Only {simpsonsOnly ? "ON" : "OFF"}
+      </button>
+      {quoteList
+        .filter(
+          (quote) => quote.character?.endsWith("Simpson") || !simpsonsOnly
+        )
+        .map((quote, index) => (
+          <QuoteCard key={index} {...quote} />
+        ))}
     </div>
   );
 }
